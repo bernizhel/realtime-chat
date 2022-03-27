538c1dc0463243d83fd652941fa1978b708d0301
Danil Kosenko <detrop.1995@gmail.com>
2022-03-27 19:27:44 +0300

    feat(server): create basic graphql-yoga configuration and variable storage

--- server/index.js
+++ server/index.js
@@ -1,1 +1,45 @@
-console.log('Realtime Chat Server');
+const { GraphQLServer } = require('graphql-yoga');
+
+const messages = [];
+
+const typeDefs = `
+    type Message {
+        id: ID!
+        user: String!
+        content: String!
+    }
+
+    type Query {
+        messages: [Message!]
+    }
+
+    type Mutation {
+        postMessage(user: String!, content: String!): ID!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        messages: () => messages,
+    },
+    Mutation: {
+        postMessage: (_, { user, content }) => {
+            const id =
+                messages
+                    .map((m) => m.id)
+                    .reduce((acc, i) => (i > acc ? i : 0), 0) + 1;
+            messages.push({ id, user, content });
+            return id;
+        },
+    },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+const start = () => {
+    server.start(({ port }) => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+};
+
+start();
